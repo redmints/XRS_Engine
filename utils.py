9eef68be305c2a8e845467fdf1fe275dae3d94cc
Mathias Hanna <mathias.hanna3@gmail.com>
2019-10-01 09:32:15 +0200

    Correction d'un bug dans la suppression d'un client dans la liste des clients actifs

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 class Utils():
+    #Constructeur
     def __init__(self):
+        #Déclaration du tableau des clients actifs
         self.clients = []
 
+    #Formatage et affichage d'un message client
     def print_log(self, ip, port, message):
         print("["+str(ip)+":"+str(port)+"] "+str(message))
 
+    #Suppression d'un client dans le tableau (déconnexion d'un client)
     def remove_client(self, ip, port):
-        for i in range(len(self.clients)):
+        for i in range(len(self.clients)-1):
             if self.clients[i][0] == ip and self.clients[i][1] == port:
                 self.clients.pop(i)
